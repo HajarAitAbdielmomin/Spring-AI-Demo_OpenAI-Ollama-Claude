adf93c12da151355da6e18afdf41d9e047dfd0cb
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2025-09-15 21:00:21 +0100

    refactor : Modify the Anthropic Api by using ChatClient instead of ChatModel

--- src/main/java/com/app/controllers/AnthropicController.java
+++ src/main/java/com/app/controllers/AnthropicController.java
@@ -2,20 +2,25 @@ package com.app.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.anthropic.AnthropicChatModel;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/anthropic")
 @CrossOrigin("*")
-@RequiredArgsConstructor
 public class AnthropicController {
-    private final AnthropicChatModel chatModel;
+    private final ChatClient chatClient;
+
+    public AnthropicController(AnthropicChatModel chatModel) {
+        this.chatClient = ChatClient.create(chatModel);
+    }
 
 
     @GetMapping("/{message}")
     public ResponseEntity<String> getAnswer(@PathVariable String message){
-        String response = chatModel.call(message);
+        String response = chatClient.prompt(message).call().content();
         return ResponseEntity.ok(response);
     }
 }
